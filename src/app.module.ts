0693f2a7e83bcf2b475e3be10ec837a7e6e73b6f
unknown <dushanse@fortude.co>
2024-10-04 10:28:07 +0530

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { EmployeeModule } from './employee/employee.module';
+import { EmployeeResolver } from './employee/employee.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'employee',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+    EmployeeModule,
+  ],
+  providers: [AppResolver],
+})
+export class AppModule {}
